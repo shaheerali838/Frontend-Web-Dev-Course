279edf1a4ce1256eac3c9634d845f4f2d2bc1401
Your Name <shaheer838838@gmail.com>
2025-09-04 22:11:34 +0500

    JS Session-1

---
+++ JavaScript/Session-1/js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello, JavaScript!");
+
+// alert("Hello, JavaScript!");
+
+// document.body.style.background = "lightblue";
+
+var a = document.body.querySelector("#button");
+
+a.addEventListener("click", () => {
+  console.log("hello");
+});
