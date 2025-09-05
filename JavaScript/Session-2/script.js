4ac62041dfa9783e2ec93b2fb35eae155dbeff01
Your Name <shaheer838838@gmail.com>
2025-09-05 22:34:02 +0500

    session 2

---
+++ JavaScript/Session-2/script.js
@@ -0,0 +1,41 @@
+// var a = 1;
+
+// let myName = "hamza";
+// console.log(myName);
+// let myName = "shaheer";
+//  myName = "shaheer";
+// console.log(myName);
+
+// const pi = 3.14;
+
+// {
+//parent
+//   {
+//child
+//     let icecream = "present";
+//     {
+//grandChild
+//     }
+//     console.log(icecream);
+//   }
+// }
+
+// let b = "shaheer " + "ali"; // concatenate if there is plu between any two datatypes like string + string OR string + integer
+// console.log(b); // ANS: shaheer ali
+
+// let a = "shaheer " - "ali"; // will be NaN if anything else rather then addition
+// console.log(a); // ANS: NaN
+
+// let c = 1 / 0; // will be Infinity if divided by 0
+// console.log(c); // ANS: Infinity
+
+// let a = "shaheer ali"; // string must alway be in double quotes
+// console.log(a); // ANS: shaheer ali
+
+// let firstName = "Shaheer";
+// let lastName;
+// let b = fName + lName; // adding 2 variables
+// console.log(`My name is ${firstName}`); // ANS: shaheer ali
+
+let fullName = "shaheer ali";
+console.log(fullName === fullName);
