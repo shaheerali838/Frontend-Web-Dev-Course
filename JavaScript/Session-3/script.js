0746409a467103f28e30e9e32b257614c5ab8e73
Your Name <shaheer838838@gmail.com>
2025-09-06 22:27:17 +0500

    Session 3- Half

---
+++ JavaScript/Session-3/script.js
@@ -0,0 +1,66 @@
+// explicit conversions because everything is done manualy
+// let a = Number("1");
+// let b = Boolean("1");
+// let c = String("1");
+
+// let a = Number(1);
+// let b = Boolean(1);
+// let c = String(1);
+
+// let a = Number(false);
+// let b = Boolean(false);
+// let c = String(false);
+
+// console.log("a " + a, typeof a);
+// console.log("b " + b, typeof b);
+// console.log("c " + c, typeof c);
+
+// implicit because conversion will happen automatically
+// let a = "5" + "5"; // 55 coz concatenation //always if there is addition and atleast 1 string is available
+// let b = 5 + 5; // 10 coz of addition
+// let c = 5 % "5"; // implicitly converted the string to number
+
+// console.log("a " + a, typeof a);
+// console.log("b " + b, typeof b);
+// console.log("c " + c, typeof c);
+
+// let a = Number(" ");
+// let b = Boolean(" ");
+// let c = String(" ");
+
+// console.log("a " + a, typeof a);
+// console.log("b " + b, typeof b);
+// console.log("c " + c, typeof c);
+
+// console.log(
+//   "is 1 in a string = to true:",
+//   ":without type checking:",
+//   1 == true
+// );
+// console.log("is 1 in a string = to true:", ":with type checking:", 1 === true);
+// console.log("b " + b, typeof b);
+// console.log("c " + c, typeof c);
+
+// Arithmetic operators
+
+//addition +
+
+console.log(9 + 1);
+
+//subtraction -
+console.log(9 - 1);
+
+//division /
+console.log(9 / 1);
+
+//multiplication *
+console.log(9 * 1);
+
+//modulus %
+console.log(9 % 1);
+
+//exponentiation **
+
+//increment ++
+
+//decrement --
