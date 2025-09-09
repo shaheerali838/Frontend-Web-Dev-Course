c5f9e347ac25fd4b16046da2e8b2c143117455bb
Your Name <shaheer838838@gmail.com>
2025-09-09 21:27:42 +0500

    Session-4

---
+++ JavaScript/Session-4/script.js
@@ -0,0 +1,77 @@
+//? 🔸 3. if / else Statement
+// let myAge = 17;
+// console.log(myAge >= 18);
+
+// if (myAge >= 18) {
+//   console.log(`yes you can drive`);
+// } else {
+//   console.log(`You cant drive yet`);
+// }
+
+// let user = prompt(`Please enter the pass`)
+// if (user === "pass") {
+//   console.log(`welcome to the dashboard`);
+// } else {
+//   console.log(`u cant access the dasboard please log in`);
+// }
+
+//? 🔸 4. Nested if / else
+// let family = 8;
+
+// if (family >= 4) {
+//   if (family >= 6) {
+//     if (family >= 8) {
+//       console.log(
+//         `ur family has 8 or more members we dont have enough space in our hotel`
+//       );
+//     } else {
+//       console.log("u have less than 8 members in your family go to 2nd floor");
+//     }
+//   } else {
+//     console.log("u have less than 6 members in your family go to 1st floor");
+//   }
+// } else {
+//   console.log(`u have less than 4 members in your family go to ground floor`);
+// }
+
+//? 🔸 5. switch Statement
+// let day = 4;
+
+// switch (day) {
+//   case 1:
+//     console.log(`its monday`);
+//     break;
+//   case 2:
+//     console.log(`its tuesday`);
+//     break;
+//   case 3:
+//     console.log(`its wednesday`);
+//     break;
+//   case 4:
+//     console.log(`its thursday`);
+//     break;
+//   case 5:
+//     console.log(`its friday`);
+//     break;
+//   case 6:
+//     console.log(`its saturday`);
+//     break;
+//   case 7:
+//     console.log(`its sunday`);
+//     break;
+//   default:
+//     console.log(`${day} is not a day`);
+//     break;
+// }
+
+//? 🔸 6. Ternary Operator (?:)
+// let myAge = 18;
+
+// myAge >= 18 ? console.log("u can drive") : console.log("u cant drive");
+
+//!🔸 comaprison of ternary and if else
+// if (myAge >= 18) {
+//   console.log(`yes you can drive`);
+// } else {
+//   console.log(`You cant drive yet`);
+// }
