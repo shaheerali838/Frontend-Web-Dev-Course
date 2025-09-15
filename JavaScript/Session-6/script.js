be71d2869f73fe0c61aa2c09979f9419c501b0fc
Your Name <shaheer838838@gmail.com>
2025-09-16 00:18:26 +0500

    Add HTML structure and JavaScript functions for Session 6; include greeting function and recap of loops

---
+++ JavaScript/Session-6/script.js
@@ -0,0 +1,74 @@
+// function sum() {
+//   console.log(1 + 1);
+// }
+
+// let a = 1,
+//   b = 2;
+// console.log(a + b);
+
+// function sum() {
+//   console.log(2 + 2);
+// }
+// sum();
+
+// let a = 1,
+//   b = 2;
+// console.log(a + b);
+// c = 3;
+// d = 4;
+// console.log(c + d);
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// sum(2, 3);
+
+// sum(6, 7);
+
+// sum(3, 4);
+
+// function calc(num1, num2, num3, num4, num5) { //these variables are parameters
+//   console.log(num1 + num2 - num3 * num4 * num5);
+
+//   return num1 + num2 - (num3 * num4) / num5;
+//    console.log(num1 + num2 - num3 * num4 * num5);
+//    console.log(num1 + num2 - num3 * num4 * num5);
+//    console.log(num1 + num2 - num3 * num4 * num5);
+//    console.log(num1 + num2 - num3 * num4 * num5);
+//    console.log(num1 + num2 - num3 * num4 * num5);
+// }
+
+// let result = calc(1, 2, 3, 4, 5);// and these values passed to the params are arguments
+
+// console.log(result);
+
+// let a = function sum(num1, num2) {
+//   console.log(num1 + num2);
+// };
+
+// let a = function sum() {
+//   console.log("Hello habibis");
+// };
+
+// a();
+
+// let a = () => {
+//   console.log("Hello habibis");
+// };
+
+// function sum() {
+//     console.log();
+// }
+
+// let sum = () => {
+//   console.log("Hello habibis");
+// };
+
+// sum();
+
+function greeting(name = "Guest") {
+  console.log(`hello ${name}`);
+}
+
+greeting("Abdul Hadi");
