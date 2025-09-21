84b870f10c59afbd989a7a5064c6665a98a1ad47
Your Name <shaheer838838@gmail.com>
2025-09-22 00:13:49 +0500

    Session-8 complete

---
+++ JavaScript/Session-7/script.js
@@ -0,0 +1,54 @@
+// function greeting() {
+//   console.log("hello habibis");
+// }
+
+// greeting();
+
+// a(); // wont work coz calling before initialization
+
+// let a = function greeting() {
+//   console.log("hello habibis");
+// };
+
+// a(); // will work
+
+// let greeting = (a, b) => a + b;
+
+// let c = greeting(10, 100);
+
+// console.log(c);
+
+// let a = (a) => {
+//   if (a > 0) {
+//     console.log("the number is positive");
+//   } else if (a < 0) {
+//     console.log("the number is negative");
+//   } else {
+//     console.log("The Number is zero");
+//   }
+// };
+
+// a(0);
+
+// let greeting = (a, b) => a * b;
+
+// let c = greeting(10, 100);
+
+// console.log(c);
+
+// let a = function greeting(name) {
+//   console.log(`hello ${name}`);
+// };
+
+// a("shaheer");
+// a("hamid");
+// a("Ibrar");
+// a("Wasim");
+
+// let a = () => {
+//   for (let index = 1; index <= 10; index++) {
+//     console.log(`${index} hello hamza`);
+//   }
+// };
+
+// a();
---
+++ JavaScript/Session-8/script.js
@@ -0,0 +1,81 @@
+// console.log(2 + 2);
+
+// console.log(typeof null);
+
+// console.log("b"-2);
+
+// console.log(typeof NaN);
+
+// {
+//     var a = 2;
+//     {
+//     let b = 3
+//     }
+//     console.log(b);
+// }
+
+//     let ABC = 2 + 2; // ABC is not equal to abc (ABC != abc)
+// console.log(abc);
+
+// let age="18"
+
+// if(age == 18){
+//     console.log("You can Drive");
+
+// }
+// let myName = "shaheer";
+// let hisName = "shaheer";
+
+// let myName = Symbol("shaheer");
+
+// let nameobj = Object(myName);
+
+// shaheer = {};
+
+// let user = {
+//   name: "Hitesh",
+//   age: 21,
+//   nameobj,
+// };
+
+// console.log(myName === hisName);
+//
+
+// var a = Symbol("shaheer");
+
+// console.log(typeof myname);
+
+// let obje1 = { name: "Hitesh", age: 20, myname };
+
+// console.log(typeof obje1.myname.description);
+
+// let myName = "shaheer";
+
+// let percentage = (totalMarks, achievedMarks) => {
+//   return (100 * achievedMarks) / totalMarks;
+// };
+
+// console.log(sum(1, 2));
+
+// console.log(percentage(1200, 853));
+
+// let MyName = new String("shaher");
+// console.log(MyName);
+
+// let user = {
+//   name: "Hitesh",
+//   age: 21,
+//   nameobj,
+// };
+
+let percentage = (totalMarks, achievedMarks) => {
+  // this log are very important dont remove coz they are checking if the i am calling this function or not
+  //   console.log("i am calling the percentage function");
+  //   console.log("is it correct that i am not calling this function");
+
+  return (100 * achievedMarks) / totalMarks;
+};
+
+percentage();
+
+percentage();
